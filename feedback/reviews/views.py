876bb932aa13d60c8e9a7b9db48787b3d5abab12
Marie Kam <marina.stozhok@gmail.com>
2025-07-27 13:38:09 -0700

    refactor: change View to FormView for ReviewView class.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,30 +2,20 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 
 from .forms import ReviewForm
 from .models import Review
 
+class ReviewView(FormView):
+   template_name = "reviews/review.html"
+   form_class = ReviewForm
+   success_url = "/submit"
 
-class ReviewView(View):
-   def get(self, request):
-      form = ReviewForm()
+   def form_valid(self, form):
+      form.save()
+      return super().form_valid(form)
 
-      return render(request, "reviews/review.html", {
-         "form": form,
-      })
-
-   def post(self, request):
-      form = ReviewForm(request.POST)
-
-      if form.is_valid():
-         form.save()
-         return HttpResponseRedirect("/submit")
-
-      return render(request, "reviews/review.html", {
-         "form": form,
-      })
 
 class SubmitView(TemplateView):
    template_name = "reviews/submit.html"
@@ -41,10 +31,12 @@ class ReviewList(ListView):
    model = Review
    context_object_name = "reviews"
 
-   def get_queryset(self):
-      base_query = super().get_queryset()
-      data = base_query.filter(rating__gte=4)
-      return data
+   # def get_queryset(self):
+   #    base_query = super().get_queryset()
+   #    data = base_query.filter(rating__gte=1).order_by(
+   #       "-rating"
+   #    )
+   #    return data
 
 class ReviewDetail(DetailView):
    template_name = "reviews/review_item.html"
